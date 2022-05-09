978815b4dc6d57559889814fd6df2f707533a3fa
davidmbedford <david.m.bedford1@gmail.com>
2022-05-09 16:02:03 -0400

    Updates

---
+++ precache-manifest.bb0960d3e31e8ffb0916c9bf7567e882.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e96c4fc94dea15e707bbfa56e3b0623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e8cbe83fea57439c8a4",
+    "url": "/static/css/main.0aeb235c.chunk.css"
+  },
+  {
+    "revision": "92899594f8d145f14570",
+    "url": "/static/js/2.29ae2f1c.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.29ae2f1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e8cbe83fea57439c8a4",
+    "url": "/static/js/main.558d315f.chunk.js"
+  },
+  {
+    "revision": "a98295522b8735dc0fe9",
+    "url": "/static/js/runtime-main.65e3c024.js"
+  },
+  {
+    "revision": "c326f8c906eb45b45ae854f0a170d75d",
+    "url": "/static/media/S.c326f8c9.png"
+  },
+  {
+    "revision": "307d83ce0d6579f061ba7ceb9902075e",
+    "url": "/static/media/SWFC.307d83ce.png"
+  },
+  {
+    "revision": "395acf36eb6532bc55f69245a6b0044a",
+    "url": "/static/media/americanstandard.395acf36.png"
+  },
+  {
+    "revision": "29930c4111d2eafea369195d2063232a",
+    "url": "/static/media/bamazon.29930c41.png"
+  },
+  {
+    "revision": "3ca3e421a74ab7bf34c199516daae12d",
+    "url": "/static/media/blitz.3ca3e421.png"
+  },
+  {
+    "revision": "19f4139c04e21d8cae908e55814899da",
+    "url": "/static/media/classiccarvalue.19f4139c.png"
+  },
+  {
+    "revision": "f78e0ad4f9c4ebdaf6d67b255f9b2ed6",
+    "url": "/static/media/cmyskills.f78e0ad4.png"
+  },
+  {
+    "revision": "3115a64f2a89807f1ead8bb4bb9419f2",
+    "url": "/static/media/friendfinder.3115a64f.png"
+  },
+  {
+    "revision": "96edb4ab54b3ddf84f840834b9545d7e",
+    "url": "/static/media/gifexploder.96edb4ab.png"
+  },
+  {
+    "revision": "3abb7e0677c82fe8e03a1619783c5aab",
+    "url": "/static/media/liri.3abb7e06.png"
+  },
+  {
+    "revision": "a3858430fdde6fdcd1cef1cd91c4bba0",
+    "url": "/static/media/liveschedule.a3858430.png"
+  },
+  {
+    "revision": "8d1b57b71e5c057d92184d3e3727e104",
+    "url": "/static/media/tc.8d1b57b7.png"
+  },
+  {
+    "revision": "72d3b419a065f6fb1981fc3bcd7179e7",
+    "url": "/static/media/thompsoncreek.72d3b419.png"
+  },
+  {
+    "revision": "245afcfcefb8d55c1375e49ba9b5d4ef",
+    "url": "/static/media/zigzag.245afcfc.png"
+  }
+]);
